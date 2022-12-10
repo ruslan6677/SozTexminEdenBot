a015e32ee6f05823ae862a8cd82ef0d78d748661
RuslanBTB <87086724+ruslan6677@users.noreply.github.com>
2022-12-10 13:48:01 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # The
-TOKEN = "5922966663:AAH8-MEn5PVQjsEj_yxQm1Tg_By9QWx7YIA"
+TOKEN = "5966826593:AAFA-ntmJ0BODj-WG_ncIQcQfrATSvAAnl8"
 with open('soz.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
